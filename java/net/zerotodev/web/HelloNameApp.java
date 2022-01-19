1222541e9d462cb65cc00b58880428aa9f76732b
parkjungkwan <pakjkwan@gmail.com>
2022-01-19 11:52:47 +0900

    2022-01-19 11:52 id pw name 앱

---
+++ java/net/zerotodev/web/HelloNameApp.java
@@ -0,0 +1,10 @@
+package net.zerotodev.web;
+
+public class HelloNameApp {
+    static String name = "철수";
+    static int age = 20;
+
+    public static void main(String[] args) {
+        System.out.print(name +"나이는 "+ age+"이다");
+    }
+}
